26d62badf7580546847c210fa3965291a43e5d37
EzeqFer <leriosos12@gmail.com>
2023-08-08 19:32:05 -0300

    Added eventos.js

---
+++ eventos.js
@@ -0,0 +1,12 @@
+
+  
+    var miDiv = document.getElementById("myBtn");
+
+    // Agregamos un controlador de eventos para el evento "click" en el div
+    miDiv.addEventListener("click", function() {
+        alert("Hola! Soy el div");
+    });
+
+    // Estilizamos el div con padding y fondo verde
+    miDiv.style.padding = "5px";
+    miDiv.style.backgroundColor = "green";
